8dcb64864cdf889e93706d5ee92383f04a5b9f66
wyq <wyq@DESKTOP-wyq>
2017-04-08 13:30:08 +0800

    Update run script function for unicode code.

--- src/org/meteoinfo/laboratory/MeteoInfoLab.java
+++ src/org/meteoinfo/laboratory/MeteoInfoLab.java
@@ -6,6 +6,7 @@
 package org.meteoinfo.laboratory;
 
 import java.awt.GraphicsEnvironment;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -38,19 +39,19 @@ public class MeteoInfoLab {
      */
     public static void main(String[] args) {
         // TODO code application logic here        
-        if (args.length >= 1){
+        if (args.length >= 1) {
             if (args[0].equalsIgnoreCase("-r")) {
                 String fontPath = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "fonts";
                 //fontPath = "D:\\MyProgram\\java\\MeteoInfoDev\\MeteoInfo\\fonts";
                 List<String> fontFns = GlobalUtil.getFiles(fontPath, ".ttc");
-                for (String fontFn : fontFns){
+                for (String fontFn : fontFns) {
                     System.out.println("Register: " + fontFn);
                     FontUtil.registerFont(fontFn);
                 }
-                args = (String[])DataConvert.resizeArray(args, args.length - 1);
+                args = (String[]) DataConvert.resizeArray(args, args.length - 1);
             }
         }
-        if (args.length >= 1) {            
+        if (args.length >= 1) {
             if (args[0].equalsIgnoreCase("-i")) {
                 runInteractive();
             } else if (args[0].equalsIgnoreCase("-b")) {
@@ -97,7 +98,7 @@ public class MeteoInfoLab {
         //state.setdefaultencoding("utf-8");
         //PythonInterpreter interp = new PythonInterpreter(null, state);
         MyPythonInterpreter interp = new MyPythonInterpreter(null, state);
-        
+
         //String pluginPath = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "plugins";
         //List<String> jarfns = GlobalUtil.getFiles(pluginPath, ".jar");
         String path = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "pylib";
@@ -112,7 +113,7 @@ public class MeteoInfoLab {
         interp.exec("mipylib.plotlib.miplot.batchmode = True");
         interp.exec("mipylib.plotlib.miplot.isinteractive = False");
         System.out.println("mipylib is loaded...");
-        try {            
+        try {
 //            File file = new File(fn);    
 //            BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
 //            StringBuilder sb = new StringBuilder();
@@ -133,12 +134,12 @@ public class MeteoInfoLab {
             System.exit(0);
         }
     }
-    
+
     private static void runScript(String args[], String fn, int idx) {
         //String ext = GlobalUtil.getFileExtension(fn);
         //registerFonts();
         org.meteoinfo.global.util.FontUtil.registerWeatherFont();
-        
+
         System.out.println("Running Jython script...");
         PySystemState state = new PySystemState();
         if (args.length > idx + 1) {
@@ -149,33 +150,45 @@ public class MeteoInfoLab {
         //state.setdefaultencoding("utf-8");
         //PythonInterpreter interp = new PythonInterpreter(null, state);
         MyPythonInterpreter interp = new MyPythonInterpreter(null, state);
-        
-        //String pluginPath = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "plugins";
-        //List<String> jarfns = GlobalUtil.getFiles(pluginPath, ".jar");
-        String path = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "pylib";
-        String toolboxPath = GlobalUtil.getAppPath(FrmMain.class) + "/toolbox";
-        interp.exec("import sys");
-        interp.exec("import os");
-        interp.exec("import datetime");
-        interp.exec("sys.path.append('" + path + "')");
-        interp.exec("from milab import *");
-        interp.exec("sys.path.append('" + toolboxPath + "')");
-        interp.exec("from toolbox import *");
-        interp.exec("mipylib.plotlib.miplot.batchmode = True");
-        interp.exec("mipylib.plotlib.miplot.isinteractive = False");
-        System.out.println("mipylib is loaded...");
-        try {            
-            File file = new File(fn);            
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            String code = new String(bytes, "utf-8");
-            //ByteArrayInputStream bis = new ByteArrayInputStream(code.getBytes());
+
+        boolean isDebug = java.lang.management.ManagementFactory.getRuntimeMXBean().
+                getInputArguments().toString().contains("jdwp");
+        String path, toolboxPath;
+        if (isDebug) {
+            path = "D:/MyProgram/java/MeteoInfoDev/MeteoInfoLab/pylib";
+            toolboxPath = "D:/MyProgram/java/MeteoInfoDev/MeteoInfoLab/toolbox";
+        } else {
+            //String pluginPath = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "plugins";
+            //List<String> jarfns = GlobalUtil.getFiles(pluginPath, ".jar");
+            path = GlobalUtil.getAppPath(FrmMain.class) + File.separator + "pylib";
+            toolboxPath = GlobalUtil.getAppPath(FrmMain.class) + "/toolbox";
+        }
+
+        try {
+            interp.exec("import sys");
+            interp.exec("import os");
+            interp.exec("import datetime");
+            interp.exec("sys.path.append('" + path + "')");
+            interp.exec("from milab import *");
+            if (!isDebug) {
+                interp.exec("sys.path.append('" + toolboxPath + "')");
+                interp.exec("from toolbox import *");
+            }
+            interp.exec("mipylib.plotlib.miplot.batchmode = True");
+            interp.exec("mipylib.plotlib.miplot.isinteractive = False");
+            System.out.println("mipylib is loaded...");
+
+            //String encoding = "utf-8";
+            //File file = new File(fn);            
+            //byte[] bytes = Files.readAllBytes(file.toPath());
+            //String code = new String(bytes, encoding);
+            //ByteArrayInputStream bis = new ByteArrayInputStream(code.getBytes(encoding));
+            //ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
             //interp.execfile(bis);
-            interp.exec(code);
-            //interp.execfile(fn);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(MeteoInfoLab.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(MeteoInfoLab.class.getName()).log(Level.SEVERE, null, ex);
+            //interp.exec(code);
+            interp.execfile(fn);
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             System.exit(0);
         }
@@ -213,7 +226,7 @@ public class MeteoInfoLab {
         }
         console.interact();
     }
-    
+
     private static void runApplication() {
         runApplication(false);
         //runApplication(true);
@@ -225,13 +238,13 @@ public class MeteoInfoLab {
             //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
             /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
             * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-            */
+             */
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
             Logger.getLogger(MeteoInfoLab.class.getName()).log(Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        
+
         //System.setProperty("-Dsun.java2d.dpiaware", "false");
 
         /* Create and display the form */
--- src/org/meteoinfo/laboratory/gui/MyPythonInterpreter.java
+++ src/org/meteoinfo/laboratory/gui/MyPythonInterpreter.java
@@ -5,6 +5,7 @@
  */
 package org.meteoinfo.laboratory.gui;
 
+import java.io.InputStream;
 import org.python.core.PyObject;
 import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
@@ -29,6 +30,22 @@ public class MyPythonInterpreter extends PythonInterpreter{
      */
     public MyPythonInterpreter(PyObject obj, PySystemState state){
         super(obj, state);
+        this.cflags.source_is_utf8 = true;
+    }
+    
+    @Override
+    public void execfile(InputStream s){
+        this.cflags.source_is_utf8 = false;
+        this.systemState.setdefaultencoding("utf-8");
+        super.execfile(s);
+        this.cflags.source_is_utf8 = true;
+    }
+    
+    @Override
+    public void execfile(String fn){
+        this.cflags.source_is_utf8 = false;
+        this.systemState.setdefaultencoding("utf-8");
+        super.execfile(fn);
         this.cflags.source_is_utf8 = true;
     }
 }
